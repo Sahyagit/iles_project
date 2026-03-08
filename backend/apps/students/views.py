f9403499d82c160be7430824d89b31fb50ab76fa
sahyagit <hasahyasharif@gmail.com>
2026-03-08 15:31:37 +0300

    students url & views

--- backend/apps/students/views.py
+++ backend/apps/students/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+
+def students_home(request):
+    return HttpResponse("Students module working")
