33670d97029d80baf6f3b9f1f71b69c3c47c28f6
sahyagit <hasahyasharif@gmail.com>
2026-03-09 18:58:09 +0300

    added user model

--- backend/apps/users/models.py
+++ backend/apps/users/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+
+    ROLE_CHOICES = (
+        ('student', 'Student'),
+        ('university_supervisor', 'University Supervisor'),
+        ('work_supervisor', 'Work Supervisor'),
+        ('admin', 'Admin'),
+    )
+
+    role = models.CharField(max_length=30, choices=ROLE_CHOICES)
+
+    phone_number = models.CharField(max_length=15, blank=True)
