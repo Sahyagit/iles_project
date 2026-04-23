826342abc2688e3fdd66b857b14250979cab5b7c
Ssebalekejohn <vicoug69@gmail.com>
2026-04-23 10:04:07 +0300

    feat: add /api/users/me/ endpoint to users urls

--- backend/apps/users/urls.py
+++ backend/apps/users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from . import views
+from .views import MeView
 
 urlpatterns = [
-    path('', views.users_home, name='users_home'),
+    path('me/', MeView.as_view(), name='user-me'),
 ]
