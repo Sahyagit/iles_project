0f24128b27b00e3c92413f8d1e6475098b9cae17
Ssebalekejohn <vicoug69@gmail.com>
2026-04-30 11:57:07 +0300

    feat: add change-password URL to users endpoints

--- backend/apps/users/urls.py
+++ backend/apps/users/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from .views import MeView, RegisterView, UserListView, UserDetailView, AdminUserListView, AdminUserDetailView
+from .views import MeView, RegisterView, UserListView, UserDetailView, ChangePasswordView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='user-register'),
     path('me/', MeView.as_view(), name='user-me'),
+    path('change-password/', ChangePasswordView.as_view(), name='change-password'),
     path('list/', UserListView.as_view(), name='user-list'),
     path('<int:pk>/', UserDetailView.as_view(), name='user-detail'),
-    path('admin/', AdminUserListView.as_view(), name='admin-users'),
-    path('admin/<int:pk>/', AdminUserDetailView.as_view(), name='admin-user-detail'),
 ]
